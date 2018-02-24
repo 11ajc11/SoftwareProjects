0c642b80a730949e298410ad28db541eb8213c5a
FLUXPUTER-PC\Fluxputer <juliogiron5@gmail.com>
2018-02-24 14:53:24 -0600

    idk why but its asking

--- employment_portal/employment_portal/settings.py
+++ employment_portal/employment_portal/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'candidates.apps.CandidatesConfig',
+    'postings.apps.PostingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
