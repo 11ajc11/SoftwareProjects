80d6b6152653a7a6189cda577f34919fd30f8cf6
Adam Cantor <11ajc11@gmail.com>
2018-03-11 23:22:54 -0500

    candidate and company routing

--- employment_portal/company/views.py
+++ employment_portal/company/views.py
@@ -3,5 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def cadmin_landing(request):
     return render(request,'cadmin_landing.html')
+
 def cadmin_edit_profile(request):
     return render(request, 'cadmin_edit_profile.html')
+
+def cadmin_add_posting(request):
+    return render(request, 'cadmin_add_posting.html')
+
+def cadmin_add_recruiter(request):
+    return render(request, 'cadmin_add_recruiter.html')
+
+def cadmin_edit_posting(request):
+    return render(request,'cadmin_edit_posting.html')
+
+def cadmin_edit_recruiter(request):
+    return render(request, 'cadmin_edit_recruiter.html')
+
+def cadmin_view_postings(request):
+    return render(request, 'cadmin_view_postings.html')
+
+def cadmin_view_recruiters(request):
+    return render(request, 'cadmin_view_recruiters.html')
