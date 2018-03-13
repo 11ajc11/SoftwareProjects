c9c2b777be097bb7223791f98c90bb9e8c91b96c
Brandon Meng <Brandon@loop-depaulsecure-237-129.depaulsecure-student.depaul.edu>
2018-03-13 11:57:29 -0500

    Revert "fixed errors from last push"
    This reverts commit 3ead2b7bd14a313d7db7be20e06b50ca44133ecf.

--- employment_portal/candidates/urls.py
+++ employment_portal/candidates/urls.py
@@ -4,7 +4,7 @@ from . import views as core_views
 app_name = 'candidates'
 urlpatterns = [
     url(r'^candidates_landing/', core_views.candidates_landing, name='candidates_landing'),
-    #url(r'^candidates_edit_profile/', core_views.candidates_edit_profile, name='candidates_edit_profile'),
+    url(r'^candidates_edit_profile/', core_views.candidates_edit_profile.as_view(), name='candidates_edit_profile'),
     url(r'^candidate_login/', core_views.candidate_login, name='candidate_login'),
     url(r'^candidate_search_companies/', core_views.candidate_search_companies, name='candidate_search_companies'),
     url(r'^candidate_search_jobs/', core_views.candidate_search_jobs, name='candidate_search_jobs'),
